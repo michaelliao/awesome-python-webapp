4d4ba06914b98843e20505fb519b340a78c1fee4
Michael Liao <askxuefeng@gmail.com>
2014-06-06 16:21:29 +0800

    build blog list page by mvvm

--- www/urls.py
+++ www/urls.py
@@ -7,13 +7,21 @@ import os, re, time, base64, hashlib, logging
 
 from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 
-from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
+from apis import api, Page, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User, Blog, Comment
 from config import configs
 
 _COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+def _get_page_index():
+    page_index = 1
+    try:
+        page_index = int(ctx.request.get('page', '1'))
+    except ValueError:
+        pass
+    return page_index
 
 def make_signed_cookie(id, password, max_age):
     # build cookie string by: id-expires-md5
@@ -63,6 +71,12 @@ def manage_interceptor(next):
     if user and user.admin:
         return next()
     raise seeother('/signin')
+
+def _get_blogs_by_page():
+    total = Blog.count_all()
+    page = Page(total, _get_page_index())
+    blogs = Blog.find_by('order by created_at desc limit ?,?', page.offset, page.limit)
+    return blogs, page
 
 @view('blogs.html')
 @get('/')
@@ -130,10 +144,25 @@ def register_user():
 def register():
     return dict()
 
+@view('manage_blog_list.html')
+@get('/manage/blogs')
+def manage_blogs():
+    return dict(page_index=_get_page_index(), user=ctx.request.user)
+
 @view('manage_blog_edit.html')
 @get('/manage/blogs/create')
 def manage_blogs_create():
     return dict(id=None, action='/api/blogs', redirect='/manage/blogs', user=ctx.request.user)
+
+@api
+@get('/api/blogs')
+def api_get_blogs():
+    format = ctx.request.get('format', '')
+    blogs, page = _get_blogs_by_page()
+    if format=='html':
+        for blog in blogs:
+            blog.content = markdown2.markdown(blog.content)
+    return dict(blogs=blogs, page=page)
 
 @api
 @post('/api/blogs')
