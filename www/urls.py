6cd7e64d243773a87c563643705e19076e976ff6
Michael Liao <askxuefeng@gmail.com>
2014-06-06 15:55:42 +0800

    use uikit and create home page

--- www/urls.py
+++ www/urls.py
@@ -3,14 +3,15 @@
 
 __author__ = 'Michael Liao'
 
-import logging
+import os, re, time, base64, hashlib, logging
 
-from transwarp.web import get, view
+from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 
 from models import User, Blog, Comment
 
-@view('test_users.html')
+@view('blogs.html')
 @get('/')
-def test_users():
-    users = User.find_all()
-    return dict(users=users)
+def index():
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
